90248f5a8a59dad88505f16d6a88a858fb1b5b49
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-27 07:45:45 -0300

    Inclusão de elementos em JS

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -24,5 +24,19 @@ for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
+
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {}
