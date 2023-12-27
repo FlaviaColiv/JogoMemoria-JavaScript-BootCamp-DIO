e7fa0956d1c9c2816f6b5eeab554fbbd38e6e187
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-27 08:04:06 -0300

    Inclusão de elementos em JavaScript

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -39,4 +39,18 @@ function handleClick() {
     }
 }
 
-function checkMatch() {}
+function checkMatch() {
+    if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert(" 🍾 🥂 Você Venceu!!! 🥂 🥳 ");
+    }
+}
